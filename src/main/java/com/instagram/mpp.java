b7c232bccd04a8f6d5a54ebd88d3b19a099d2429
pavanja <pavanja@gmail.com>
2023-03-21 06:32:58 +0530

    second commit

---
+++ src/main/java/com/instagram/mpp.java
@@ -0,0 +1,13 @@
+package com.instagram;
+
+/**
+ * Hello world!
+ *
+ */
+public class mpp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Worlds!" );
+    }
+}
